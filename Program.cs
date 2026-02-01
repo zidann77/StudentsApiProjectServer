e4bfcb71f4c15e2170edf7432020eddab59df86e
zidann77 <azeidan061@gmail.com>
2026-02-02 00:32:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjectServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //   Console.WriteLine("Hello, World!");
+         Console.ReadLine();
+        }
+    }
+}
